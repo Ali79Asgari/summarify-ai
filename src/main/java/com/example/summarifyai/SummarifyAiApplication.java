3b2af3cebf7afc84624db8446ead4a5e6f8f987d
Ali Asgari <ali79asgari@gmail.com>
2025-08-15 03:50:52 +0330

    feat: initialize project

---
+++ src/main/java/com/example/summarifyai/SummarifyAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.summarifyai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummarifyAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummarifyAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/summarifyai/SummarifyAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.summarifyai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummarifyAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
